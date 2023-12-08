49ecfa56a522d4d40b20bffa494866e6ff89e557
kirill_krutoi <92685566+natilinalku@users.noreply.github.com>
2023-12-08 11:42:16 +0500

    Add files via upload

--- j.js
+++ j.js
@@ -24,11 +24,42 @@ function P(n,k) {
 t['P']=P
 t['A']=A
 t['C']=C
+typ=true
 window.addEventListener('load',function() {
+document.getElementById('kom').hidden=!document.getElementById('kom').hidden
 const button = document.getElementById("bu")
-const outp = document.getElementById('e')
+const outp = document.getElementById('eee')
+const met=document.querySelector('input[value="ko"]')
+const no=document.querySelector('input[value="m"]')
+no.addEventListener('change',function() {
+    document.getElementById('e').hidden=!document.getElementById('e').hidden
+    document.getElementById('kom').hidden=!document.getElementById('kom').hidden
+    typ=!typ
+    document.getElementById('n').placeholder='корзинки'
+    document.getElementById('k').placeholder='шары'
+})
+met.addEventListener('change',function() {
+    document.getElementById('kom').hidden=!document.getElementById('kom').hidden
+    document.getElementById('e').hidden=!document.getElementById('e').hidden
+    typ=!typ
+    document.getElementById('n').placeholder='n'
+    document.getElementById('k').placeholder='k'
+})
 button.addEventListener('click',function (){
-const sel =document.querySelector('input[name="b"]:checked')
-outp.value=t[sel.value](document.getElementById('n').value,document.getElementById('k').value)
+if (typ==true){
+    const sel =document.querySelector('input[name="b"]:checked')
+    outp.value=Math.round(t[sel.value](document.getElementById('n').value,document.getElementById('k').value))
+} else{
+    const tel =document.querySelector('input[name="c"]:checked')
+    if (tel.value=='e1'){
+        outp.value=Math.round(t['C'](document.getElementById('n').value-1,document.getElementById('k').value-1))
+    }
+    if (tel.value=='e2'){
+        outp.value=Math.round(t['C']((Number(document.getElementById('n').value)+Number(document.getElementById('k').value)-1),document.getElementById('k').value-1))
+    }else{
+        outp.value=Math.round(t['C'](document.getElementById('n').value,document.getElementById('k').value))
+    }
+}
+
 })
 })
